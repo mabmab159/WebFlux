4ba14e4c808789d44ae1051204deac1d4934d970
MabMab^^ <miguelberrioh@hotmail.com>
2023-10-04 16:13:30 -0500

    Agregando WebFilter

---
+++ src/main/java/com/miguel/webflux/filter/ExampleFilter.java
@@ -0,0 +1,16 @@
+package com.miguel.webflux.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class ExampleFilter implements WebFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        exchange.getResponse().getHeaders().add("user", "mitocode");
+        return chain.filter(exchange);
+    }
+}
